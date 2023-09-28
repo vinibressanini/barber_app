bd8e1514b9fffa786fa5f27f574073f50140110c
vinibressanini <vinimarcus41@gmail.com>
2023-09-28 18:03:02 -0300

    feat : implemented user register repository and service

---
+++ lib/src/services/user_register/user_register_service_impl.dart
@@ -0,0 +1,28 @@
+import 'package:barber_app/src/core/exceptions/service_exception.dart';
+import 'package:barber_app/src/core/fp/either.dart';
+import 'package:barber_app/src/core/fp/nil.dart';
+import 'package:barber_app/src/repositories/user/user_repository.dart';
+import 'package:barber_app/src/services/user_login/user_login_service.dart';
+import 'package:barber_app/src/services/user_register/user_register_service.dart';
+
+class UserRegisterServiceImpl implements UserRegisterService {
+  final UserRepository userRepository;
+  final UserLoginService userLoginService;
+
+  UserRegisterServiceImpl(
+      {required this.userRepository, required this.userLoginService});
+
+  @override
+  Future<Either<ServiceException, Nil>> execute(
+      ({String email, String name, String password}) userData) async {
+    final registerResult = await userRepository.registerAdmin(userData);
+
+    switch (registerResult) {
+      case Success():
+        return await userLoginService.execute(
+            userData.email, userData.password);
+      case Failure(:final exception):
+        return Failure(ServiceException(exception.message));
+    }
+  }
+}
